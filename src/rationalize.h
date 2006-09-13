4a55aa37222c87165bfcee653a0f332e0a1d0a0b
polyomino <polyomino@b954413a-2f10-0410-8e2d-f716d4172451>
2006-09-13 09:59:07 +0000

    new
    git-svn-id: http://hexaflexagon.com/src/qt_tools@130 b954413a-2f10-0410-8e2d-f716d4172451

---
+++ src/rationalize.h
@@ -0,0 +1,16 @@
+/*
+ *  rationalize.h
+ *
+ *  Created by david van brink on 9/13/06.
+ *  Copyright 2006 omino.com. All rights reserved.
+ *
+ */
+
+void rationalize(double n,
+        unsigned long long numeratorMax,
+        unsigned long long denominatorMax,
+        unsigned long long *numeratorOut,
+        unsigned long long *denominatorOut);
+		
+// the end
+
